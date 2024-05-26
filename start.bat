d833416e1838ff0864369119c54ba564513eb039
Rolaski <jakubowskijjal1@gmail.com>
2024-05-27 00:07:36 +0200

    Update start.bat

--- start.bat
+++ start.bat
@@ -4,13 +4,6 @@ if %errorlevel% neq 0 msg %username% "Nie udalo sie utworzyc bazy danych." && ex
 
 php -r "copy('.env.example', '.env');"
 
-powershell -Command "(Get-Content .env) -replace 'DB_CONNECTION=.*', 'DB_CONNECTION=mysql' | Set-Content .env"
-powershell -Command "(Get-Content .env) -replace 'DB_HOST=.*', 'DB_HOST=127.0.0.1' | Set-Content .env"
-powershell -Command "(Get-Content .env) -replace 'DB_PORT=.*', 'DB_PORT=3306' | Set-Content .env"
-powershell -Command "(Get-Content .env) -replace 'DB_DATABASE=.*', 'DB_DATABASE=zooland_db' | Set-Content .env"
-powershell -Command "(Get-Content .env) -replace 'DB_USERNAME=.*', 'DB_USERNAME=root' | Set-Content .env"
-powershell -Command "(Get-Content .env) -replace 'DB_PASSWORD=.*', 'DB_PASSWORD=' | Set-Content .env"
-
 call composer install
 
 call php artisan migrate

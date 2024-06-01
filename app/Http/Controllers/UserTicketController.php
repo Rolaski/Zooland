53af4ba5b56cf21ba52579e360fcda191ee2cbb5
Rolaski <jakubowskijjal1@gmail.com>
2024-06-01 15:17:46 +0200

    delete comments

--- app/Http/Controllers/UserTicketController.php
+++ app/Http/Controllers/UserTicketController.php
@@ -20,14 +20,12 @@ class UserTicketController extends Controller
 
     public function store(Request $request)
     {
-        // Walidacja danych
         $request->validate([
             'reservation_id' => 'required|numeric',
             'ticket_id' => 'required|numeric',
             'quantity' => 'required|numeric|min:1',
         ]);
 
-        // Sprawdzenie, czy istnieje rezerwacja o podanym ID
         $reservationExists = Reservation::where('id', $request->reservation_id)->exists();
 
         if (!$reservationExists) {
@@ -36,7 +34,6 @@ class UserTicketController extends Controller
             ]);
         }
 
-        // Utwórz nowy bilet użytkownika
         TicketUser::create([
             'reservation_id' => $request->reservation_id,
             'ticket_id' => $request->ticket_id,
@@ -48,14 +45,11 @@ class UserTicketController extends Controller
 
     public function update(Request $request, $id)
     {
-        // Walidacja danych
         $request->validate([
             'ticket_id' => 'required|numeric',
             'quantity' => 'required|numeric|min:1',
         ]);
 
-
-        // Znajdź i zaktualizuj bilet użytkownika
         $user_ticket = TicketUser::findOrFail($id);
         $user_ticket->update([
             'ticket_id' => $request->ticket_id,
@@ -66,22 +60,18 @@ class UserTicketController extends Controller
     }
 
     public function destroy($id)
-{
-    // Znajdź i usuń bilet użytkownika
+    {
     $user_ticket = TicketUser::findOrFail($id);
     $user_ticket->delete();
 
-    // Sprawdź, czy dla rezerwacji nie ma już żadnych biletów
     $reservation_id = $user_ticket->reservation_id;
     $tickets_count = TicketUser::where('reservation_id', $reservation_id)->count();
 
     if ($tickets_count === 0) {
-        // Usuń rezerwację, jeśli nie ma już przypisanych do niej biletów
-        // (zakładając, że masz model dla rezerwacji o nazwie Reservation)
         Reservation::where('id', $reservation_id)->delete();
     }
 
     return redirect()->route('admin.user_tickets.index')->with('success', 'User ticket deleted successfully!');
-}
+    }
 
 }
